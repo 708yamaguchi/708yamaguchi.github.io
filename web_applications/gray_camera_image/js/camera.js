a9d4fcc5097149e549ffd7aef1fcbd07e414e7b6
Naoya Yamaguchi <708yamaguchi@gmail.com>
2020-06-13 22:08:11 +0900

    rebuilding site 2020年  6月 13日 土曜日 22:08:11 JST

--- web_applications/gray_camera_image/js/camera.js
+++ web_applications/gray_camera_image/js/camera.js
@@ -1,14 +1,16 @@
 window.onload = () => {
     const video  = document.querySelector("#camera");
-    const canvas = document.querySelector("#picture");
+    video.style.display = 'none'; // Do not show video
+    const canvas = document.querySelector("#orig");
+    const canvas_p = document.querySelector("#processed");
     const se     = document.querySelector('#se');
 
     /** カメラ設定 */
     const constraints = {
         audio: false,
         video: {
-            width: 300,
-            height: 200,
+            // width: video.videoWidth,
+            // height: video.videoHeight,
             facingMode: "user"   // フロントカメラを利用する
             // facingMode: { exact: "environment" }  // リアカメラを利用する場合
         }
@@ -24,12 +26,36 @@ window.onload = () => {
         .catch( (err) => {
             console.log(err.name + ": " + err.message);
         });
+    // 再生可能な状態になったら自動で再生する
+    video.addEventListener('canplaythrough', function(){
+		video.play();
+    }, false);
+    // 再生可能時のイベントリスナーを登録
+    video.addEventListener('canplaythrough', function(){
+        const ctx = canvas.getContext("2d");
+        // 33ミリ秒毎にcanvasに動画をコピーする
+        const videoW = video.videoWidth;
+        const videoH = video.videoHeight;
+        var ratio = 0;
+        if (videoW > videoH) {
+            ratio = 300.0 / videoW;
+        }
+        else {
+            ratio = 300.0 / videoH;
+        }
+        setInterval(function(){
+            ctx.drawImage(video, 0, 0, videoW, videoH,
+                          (300.0 / 2) - (videoW * ratio / 2), (300.0 / 2) - (videoH * ratio / 2),
+                          videoW * ratio, videoH * ratio);
+        }, 33);
+    }, false);
+
 
     /**
      * シャッターボタン
      */
     document.querySelector("#shutter").addEventListener("click", () => {
-        const ctx = canvas.getContext("2d");
+        const ctx_p = canvas_p.getContext("2d");
 
         // 演出的な目的で一度映像を止めてSEを再生する
         video.pause();  // 映像を停止
@@ -39,10 +65,25 @@ window.onload = () => {
         }, 500);
 
         // canvasに画像を貼り付ける
-        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        const videoW = video.videoWidth;
+        const videoH = video.videoHeight;
+        var ratio = 0;
+        if (videoW > videoH) {
+            ratio = 300.0 / videoW;
+        }
+        else {
+            ratio = 300.0 / videoH;
+        }
+        ctx_p.drawImage(video, 0, 0, videoW, videoH,
+                        (300.0 / 2) - (videoW * ratio / 2), (300.0 / 2) - (videoH * ratio / 2),
+                        videoW * ratio, videoH * ratio);
 
         // ImageDataの生成
         // var source = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        var source = ctx_p.getImageData((300.0 / 2) - (videoW * ratio / 2),
+                                        (300.0 / 2) - (videoH * ratio / 2),
+                                        videoW * ratio,
+                                        videoH * ratio);
 
         // // For effect.js
         // // エフェクトの実行
@@ -51,13 +92,19 @@ window.onload = () => {
         // // ctx.putImageData(source,0,0);
 
         // // For OpenCV.js
-        // let src = cv.imread("picture");
-        // let dst = new cv.Mat();
-        // cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY, 0);
-        // // cv.threshold(dst, dst, 100, 255, cv.THRESH_OTSU);
-        // cv.imshow("picture", dst);
-        // src.delete();
-        // dst.delete();
+        let src = cv.imread("orig");
+        // let rect = new cv.Rect((300.0 / 2) - (videoW * ratio / 2),
+        //                        (300.0 / 2) - (videoH * ratio / 2),
+        //                        (300.0 / 2) + (videoW * ratio / 2),
+        //                        (300.0 / 2) + (videoH * ratio / 2))
+        // src = src.roi(rect);
+        let dst = new cv.Mat();
+        // dst = dst.roi(rect);
+        cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY, 0);
+        // cv.threshold(dst, dst, 100, 255, cv.THRESH_OTSU);
+        cv.imshow("processed", dst);
+        src.delete();
+        dst.delete();
 
     });
 };
