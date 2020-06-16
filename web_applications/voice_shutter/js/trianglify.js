5fea7efb7e2951413affde845acd9f9944bbe7c2
Naoya Yamaguchi <708yamaguchi@gmail.com>
2020-06-16 18:47:07 +0900

    rebuilding site 2020年  6月 16日 火曜日 18:47:07 JST

--- web_applications/voice_shutter/js/trianglify.js
+++ web_applications/voice_shutter/js/trianglify.js
@@ -5,7 +5,7 @@ function addTriangleTo(target) {
     var dimensions = target.getClientRects()[0];
     var pattern = Trianglify({
         width: dimensions.width,
-        height: dimensions.height + 400,
+        height: dimensions.height + 1000,
         cell_size: 75,
         x_colors: 'GnBu',
         palette: Trianglify.colorbrewer,
